fd99c5b953b43db5c94320ee5d55446cfa74ba1d
isaacmoriel <isaacmoriel@yahoo.com>
2022-02-22 20:04:53 -0700

    added Registraiton Module

---
+++ CodeSampling/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+let modules = [];
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports:[
+
+  ],
+  declarations: []
+})
+export class SharedModule { }
