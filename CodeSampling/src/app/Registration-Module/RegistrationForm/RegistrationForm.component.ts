b50b1069e3cbaf74aaeacfdf52e439fb58774c90
isaacmoriel <isaacmoriel@yahoo.com>
2022-02-22 20:13:02 -0700

    added registration and login module

---
+++ CodeSampling/src/app/Registration-Module/RegistrationForm/RegistrationForm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-RegistrationForm',
+  templateUrl: './RegistrationForm.component.html',
+  styleUrls: ['./RegistrationForm.component.css']
+})
+export class RegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- CodeSampling/src/app/app-routing.module.ts
+++ CodeSampling/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationFormComponent } from './Registration-Module/RegistrationForm/RegistrationForm.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'**',component:RegistrationFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
